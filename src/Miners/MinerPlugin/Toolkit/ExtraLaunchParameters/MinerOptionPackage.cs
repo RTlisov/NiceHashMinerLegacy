5a1e28def1844ca185a7ca4a6e0666a1fab3abcd
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-03-18 14:36:35 +0100

    corner stone for extra launch parameters

---
+++ src/Miners/MinerPlugin/Toolkit/ExtraLaunchParameters/MinerOptionPackage.cs
@@ -0,0 +1,28 @@
+﻿using NiceHashMinerLegacy.Common.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace MinerPlugin.Toolkit.ExtraLaunchParameters
+{
+    public class MinerOptionPackage
+    {
+        public bool OverrideUserSettings;
+        public List<MinerOption> GeneralOptions;
+        public List<MinerOption> TemperatureOptions;
+
+        public MinerOptionPackage(bool iOverrideSettings, List<MinerOption> iGeneralOptions, List<MinerOption> iTemperatureOptions)
+        {
+            OverrideUserSettings = iOverrideSettings;
+            GeneralOptions = iGeneralOptions;
+            TemperatureOptions = iTemperatureOptions;
+        }
+
+        public MinerOptionPackage(List<MinerOption> iGeneralOptions)
+            : this(false, iGeneralOptions, new List<MinerOption>()) //default override is false?
+        { }
+
+        // For JSON deserialization
+        public MinerOptionPackage()
+        { }
+    }
+}
